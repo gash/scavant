8c99ced68fd19cd6dc254841ab16bb94956095a2
Isaac Wolkerstorfer <agnoster@gmail.com>
2011-02-06 19:10:13 +0100

    Initial Rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scavant::Application.config.secret_token = '838d804cc34af2f342679cb831648efbcf9d20192579d20a87c0407b2b94c6ef8388671008b7cd6941411a2041736f95f57a614ae3f98d4b682f1e8d3cc276ac'
